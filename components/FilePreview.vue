fd1d7cbaf959c10f5741bdc4aaa69e6c9a3f20d0
Markus Faessler <markus.faessler@abacus.ch>
2024-02-24 15:51:54 +0100

    feat: create component FilePreview

---
+++ components/FilePreview.vue
@@ -0,0 +1,11 @@
+<script setup>
+const content = ref(null);
+defineExpose({ content });
+</script>
+
+<template>
+    <div ref="content">
+        <h1>This is a sample pdf</h1>
+        <p>Hello World</p>
+    </div>
+</template>
