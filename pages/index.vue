69150fd89d7bd434226dbb5329d8907ab51f0b12
Markus Faessler <markus.faessler@abacus.ch>
2024-02-24 13:43:49 +0100

    feat: add simple nuxt ui button

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
     <div>
         <h1>Home Page</h1>
         <p>This is a sample</p>
+        <UButton @click="() => console.log('You clicked the button')">Click me</UButton>
     </div>
 </template>
