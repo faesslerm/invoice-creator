87807ec097ff182ad965bf27e657bf57a6f202c3
Markus Faessler <markus.faessler@abacus.ch>
2024-02-24 14:45:52 +0100

    feat: generate sample pdf

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+import { jsPDF } from "jspdf";
+
+function createPdf(event) {
+    console.log("Generating pdf...");
+    // Default export is a4 paper, portrait, using millimeters for units
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("sample_a4.pdf");
+}
+</script>
+
 <template>
     <div>
         <h1>Home Page</h1>
         <p>This is a sample</p>
-        <UButton @click="() => console.log('You clicked the button')">Click me</UButton>
+        <UButton @click="createPdf">Generate PDF</UButton>
         <PersonForm />
     </div>
 </template>
