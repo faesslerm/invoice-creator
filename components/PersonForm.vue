1e5106438a8691a260971fdd00c2c3b822cdd050
Markus Faessler <markus.faessler@abacus.ch>
2024-02-24 14:28:43 +0100

    feat: create component PersonForm

---
+++ components/PersonForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '#ui/types'
+import { reactive } from 'vue';
+
+const state = reactive({
+  firstname: undefined,
+  lastname: undefined,
+  street: undefined,
+  streetNumber: undefined,
+})
+
+async function onSubmit (event: FormSubmitEvent<any>) {
+  // Do something with data
+  console.log(event.data)
+}
+</script>
+
+<template>
+  <UForm :state="state" class="space-y-4 w-64 p-4 shadow" @submit="onSubmit">
+    <UFormGroup label="Firstname" name="firstname">
+      <UInput v-model="state.firstname" />
+    </UFormGroup>
+
+    <UFormGroup label="Lastname" name="lastname">
+      <UInput v-model="state.lastname" />
+    </UFormGroup>
+
+    <UFormGroup label="Street" name="street">
+      <UInput v-model="state.street" />
+    </UFormGroup>
+
+    <UFormGroup label="Street number" name="streetNumber">
+      <UInput v-model="state.streetNumber" type="number"/>
+    </UFormGroup>
+
+    <UButton type="submit">
+      Submit
+    </UButton>
+  </UForm>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,6 @@
         <h1>Home Page</h1>
         <p>This is a sample</p>
         <UButton @click="() => console.log('You clicked the button')">Click me</UButton>
+        <PersonForm />
     </div>
 </template>
