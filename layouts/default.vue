31d183ef09aa1777ea77be3476f274301194b386
Markus Faessler <markus.faessler@abacus.ch>
2024-03-02 09:37:18 +0100

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="header shadow-sm mb-6">
+            <p>Invoice Creator</p>
+        </div>
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.header {
+    padding: 1rem 2rem;
+}
+
+.header p {
+    font-size: 1.5rem;
+    font-weight: 800;
+}
+</style>
