9c487035e61ce9901d579ed3e2c48b5cb7c68f12
Markus Faessler <markus.faessler@abacus.ch>
2024-03-10 16:34:28 +0100

    feat: add height/width to filepreview

--- components/FilePreview.vue
+++ components/FilePreview.vue
@@ -4,8 +4,18 @@ defineExpose({ content });
 </script>
 
 <template>
-    <div ref="content">
+    <div ref="content" class="invoice border-slate-600 rounded p-4">
         <h1>This is a sample pdf</h1>
         <p>Hello World</p>
     </div>
 </template>
+
+<style scoped>
+.invoice {
+    width: 210mm;
+    min-width: 210mm;
+    height: 297mm;
+    min-height: 297mm;
+    background-color: rgb(241, 241, 241);
+}
+</style>
